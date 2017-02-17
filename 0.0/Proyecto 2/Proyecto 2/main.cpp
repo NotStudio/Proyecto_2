a9e7b2427fba2331935fd14212e8c8aa86be0f26
Manuel Hernández <manherna@ucm.es>
2017-02-17 12:13:22 +0100

    SDL añadido. Con dll y todo

---
+++ 0.0/Proyecto 2/Proyecto 2/main.cpp
@@ -0,0 +1,8 @@
+#include <SDL.h>
+
+
+int main(int argc, char*[]) {
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Quit();
+	return 0;
+}
--- Juegaso/Project/Source.cpp
+++
@@ -1,7 +0,0 @@
-#include <SDL.h>
-
-int main(int argc, char*[]) {
-	SDL_Init(SDL_INIT_EVERYTHING);
-	SDL_Quit();
-	return 0;
-}
