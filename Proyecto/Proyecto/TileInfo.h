1b5a78eb56675ddb5c021f21bba2dc8b87229245
franlbc97 <franlbc97@gmail.com>
2017-04-03 20:35:47 +0200

    arreglao

--- Proyecto/Proyecto/TileInfo.h
+++ Proyecto/Proyecto/TileInfo.h
@@ -9,11 +9,11 @@ enum TiposTile
 	S5 = 4, S6 = 5, S7 = 6, S8 = 7,
 	S9 = 8, S10 = 9, S11 = 10, S12 = 11,
 	PO = 12, PN=13, PS = 14, PE = 15,
-	ENO = 16, ENE = 17, ESE = 18, ESO = 19,
-	INO = 20, INE = 21, ISO = 22, ISE = 23,
+	ENO = 16, ENE = 17, ESO = 18, ESE = 19,
+	INO = 20, INE = 21, ISE = 22, ISO = 23,
 	MH = 24, MV= 25, PUN = 26, PUE = 27,
 	PUO = 28, PUS = 29, CRZ = 30, TI = 31,
-	T = 32, TE = 33, TO = 34, NOT = 35,
+	T = 32, TE = 33, TO = 34, ROC = 35,
 };
 
 //Norte = 0, Este = 1, Sur = 2, Oeste = 3, Sinsitio = 4,
