5ac7536a6fe8fa1a1a812c2803a42b46b29864c6
pachii <carsan15@ucm.es>
2017-05-16 12:26:37 +0200

    Añadidos diferentes sprites, retocado cosas

--- Proyecto/Proyecto/ZonaBosque.cpp
+++ Proyecto/Proyecto/ZonaBosque.cpp
@@ -6,13 +6,15 @@ ZonaBosque::ZonaBosque(Juego* punteroJuego) :ZonaAccion(punteroJuego)
 	pJuego->cambiarMusica("are");
 	id = "zon1";
 
-	setHabitaciones(10, 5);
+	setHabitaciones(2, 5);
 	for (size_t i = 0; i < niveles->size(); i++)
 	{
 		niveles->at(i)->InitRoom();
 		niveles->at(i)->abrirPuertas();
 	}
 	setNivelActual();
+	
+	//pJuego->getCamera()->setLimite({ pJuego->getCameraRect().x, pJuego->getCameraRect().y, pJuego->getCameraRect().w + 500, pJuego->getCameraRect().h + 500 });
 
 }
 
--- Proyecto/Proyecto/ZonaDesguace.cpp
+++ Proyecto/Proyecto/ZonaDesguace.cpp
@@ -6,7 +6,7 @@ ZonaDesguace::ZonaDesguace(Juego* punteroJuego) :ZonaAccion(punteroJuego)
 	pJuego->cambiarMusica("are");
 	id = "zon2";
 
-	setHabitaciones(9,4);
+	setHabitaciones(1,4);
 	initRooms();
 	setNivelActual();
 }
