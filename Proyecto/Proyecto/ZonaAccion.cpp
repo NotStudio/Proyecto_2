806c89c41c34f36d8dd48f6aae371ff170b2610c
Ángel <skinnyrome@outlook.es>
2017-05-23 12:04:12 +0200

    Merge done

--- Proyecto/Proyecto/ZonaAccion.cpp
+++ Proyecto/Proyecto/ZonaAccion.cpp
@@ -160,7 +160,7 @@ void ZonaAccion::setHabitaciones(int NumeroHabitaciones, int cadaCuantasHabitaci
 			BuildRoom(Normal);
 		}
 	}
-	BuildIniRoom();
+	
 	BuildRoom(Boss);
 	BuildFinRoom();
 }
--- Proyecto/Proyecto/ZonaBosque.cpp
+++ Proyecto/Proyecto/ZonaBosque.cpp
@@ -6,7 +6,8 @@ ZonaBosque::ZonaBosque(Juego* punteroJuego) :ZonaAccion(punteroJuego)
 	pJuego->cambiarMusica("are");
 	id = "zon1";
 
-	setHabitaciones(1,1);	initRooms();
+	setHabitaciones(5,3);	
+	initRooms();
 	setNivelActual();
 	
 	//pJuego->getCamera()->setLimite({ pJuego->getCameraRect().x, pJuego->getCameraRect().y, pJuego->getCameraRect().w + 500, pJuego->getCameraRect().h + 500 });
