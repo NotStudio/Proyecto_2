875be6b1e96f41412c71524106404c6e2001ae71
Ángel <skinnyrome@outlook.es>
2017-05-25 14:08:39 +0200

    Primera versión tutorial

--- Proyecto/Proyecto/ZonaTutorial.h
+++ Proyecto/Proyecto/ZonaTutorial.h
@@ -4,13 +4,19 @@
 class ZonaTutorial :
 	public ZonaAccion
 {
-	TextoSDL texto_;
+	TextoSDL texto1_;
+	TextoSDL texto2_;
+	string frase1_, frase2_;
+	float timerTexto_;
 
 	void setTextos();
+
+	void updateTextos();
 public:
 	ZonaTutorial(Juego* j);
 	virtual ~ZonaTutorial();
 
 	virtual void draw();
+	virtual void update();
 };
 #endif //ZONA_TUTORIAL_H
